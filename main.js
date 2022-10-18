a883ee628b0c18e0dd5475360472d031d8354c65
sjulin93 <sjulin.simon@gmail.com>
2022-10-18 16:03:29 +0200

    testar

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 /* Kod för menyn*/
+
+//toggle funktion för ta bort fiskrätter
+$("#fish").on("click", function(){
+    $(".fisk").slideToggle();
+});
+
+//toggle funktion för ta bort kötträtter
+$("#kött").on("click", function(){
+    $(".kött").slideToggle();
+});
+//funtion för nötter
+$("#nuts").on("click", function(){
+    $(".nötter").slideToggle();
+});
+//gluten
+$("#gluten").on("click", function(){
+    $(".gluten").slideToggle();
+});
+//laktos
+$("#lactose").on("click", function(){
+    $(".laktos").slideToggle();
+});
